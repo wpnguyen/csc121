9712d2e75a5b720c904d6c1f4dfcf43abca519ba
wpnguyen <pnguyen4@student.holmescc.edu>
2020-01-22 13:47:20 -0600

    inclass

--- chapter3.py
+++ chapter3.py
@@ -1,3 +1,10 @@
 import arcade
 arcade.open_window(600, 600, "Drawing Example")
+
+arcade.set_background_color(arcade.color.AIR_SUPERIORITY_BLUE)
+
+arcade.start_render()
+
+arcade.finish_render()
+
 arcade.run()
