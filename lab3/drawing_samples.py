f1ccd5366bf5c9b07f87db29fbf0e32541cf1f3c
wpnguyen <pnguyen4@student.holmescc.edu>
2020-01-22 12:37:56 -0600

    inclass

---
+++ lab3/drawing_samples.py
@@ -0,0 +1,16 @@
+"""
+This is a sample program to show how to draw using the Python programing
+language and the Arcade library.
+"""
+
+# Import the "arcade" library
+import arcade
+
+
+
+
+
+arcade.open_window(600, 600, "Drawing Example")
+
+
+arcade.run()
